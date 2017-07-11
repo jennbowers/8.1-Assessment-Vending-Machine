e643e78f28133a096a8d9ef56bfbbe6f9a398843
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-11 16:23:43 -0400

    did tests to add items to mongodb and then delete them after

--- test.js
+++ test.js
@@ -5,7 +5,25 @@ const Customer = require('./models/customers');
 const Vendor = require('./models/vendors');
 
 
-describe('basic model tests', function() {
+describe('basic vendor tests', function() {
+  afterEach(function(done) {
+    Vendor.deleteMany({}).then(done());
+  });
+
+  it('can create a vendor item in the db and find it with mongoose', function(done) {
+    const vendor = new Vendor({item: 'Coke', quantity: 2, cost: 50}).save().then(function(newVendor) {
+      expect(newVendor.item).to.equal('Coke');
+      expect(newVendor.quantity).to.equal(2);
+      expect(newVendor.cost).to.equal(50);
+      done();
+    });
+  });
+});
+
+describe('basic customer tests', function() {
+  afterEach(function(done) {
+    Customer.deleteMany({}).then(done());
+    });
 
   it('can create a customer item in the db and find it with mongoose', function(done) {
     const customer = new Customer({item: 'Coke', quantity: 10, cost: 50}).save().then(function(newCustomer) {
@@ -16,44 +34,6 @@ describe('basic model tests', function() {
     });
   });
 });
-
-// describe('basic model tests', function() {
-//   // learning about hooks in mocha
-//   // it should be before the it, but after the describe
-//   beforeEach(function(done) {
-//     Cat.deleteMany({}).then(done());
-//   });
-//
-//   // another type of hook
-//   afterEach(function(done) {
-//     Cat.deleteMany({}).then(done());
-//   });
-//
-//   it('test should clean up after itself', function(done) {
-//     // creates a new cat because we know we can
-//     const cat = new Cat().save().then(function(newCat) {
-//       // count all the cats in the Cats database
-//       Cat.count().then(function(count) {
-//         expect(count).to.equal(1);
-//         done();
-//       });
-//
-//     });
-//   });
-//
-//   // creating a model in the database and set attributes
-//   it('can create a cat in the db and find it with the mongo syntax', function(done) {
-//     // ensuring no typos and that something happens
-//     // expect(5).to.equal(4);
-//     const cat = new Cat({name: 'mittens', fluffiness: 10})
-//     .save().then(function(newCat) {
-//       expect(newCat.name).to.equal('mittens');
-//       expect(newCat.fluffiness).to.equal(10);
-//       done();
-//     });
-//   });
-// });
-
 
 describe('basic api endpoint tests', function() {
   it('can access api endpoints successfully', function(done) {
